7e0137dc894964a05ed2f49a0846b455b781d054
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-06 08:22:02 +0000

    added routing to the mainpage and notepad page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { NotepadPageComponent } from './notepad-page/notepad-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent, children: [
+    {path:'', component: NotepadPageComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
