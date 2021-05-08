0bf040a9f7b1556d89dc91140e59df68f7131a71
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-08 00:19:34 +0000

    search bar done

--- src/app/notepad-page/notepad-page.component.ts
+++ src/app/notepad-page/notepad-page.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotepadPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+// const input:any = document.querySelector(".finder__input");
+// const finder:any = document.querySelector(".finder");
+// const form:any = document.querySelector("form");
+
+// input.addEventListener("focus", () => {
+//   finder.classList.add("active");
+// });
+
+// input.addEventListener("blur", () => {
+//   if (input.value.length === 0) {
+//     finder.classList.remove("active");
+//   }
+// }); 
+
+// form.addEventListener("submit", (ev) => {
+//   ev.preventDefault();
+//   finder.classList.add("processing");
+//   finder.classList.remove("active");
+//   input.disabled = true;
+//   setTimeout(() => {
+//     finder.classList.remove("processing");
+//     input.disabled = false;
+//     if (input.value.length > 0) {
+//       finder.classList.add("active");
+//     }
+//   }, 1000);
+// });
+   }
 
   ngOnInit(): void {
   }
