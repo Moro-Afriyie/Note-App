0afe2b916d21f1b0cb851a6d19e0bf664090bcb4
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-09 09:13:10 +0000

    added a NoteDetailsModule class to the note.model.ts file

--- src/app/Services/note-pad-edit-service.service.ts
+++ src/app/Services/note-pad-edit-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {NoteDetailsModule } from '../models/note.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotePadEditServiceService {
 
-  constructor() { }
+  // fields
+  private noteDetails: NoteDetailsModule;
+  private note: Date;
+  //Date array
+  private monthsArr = ['Jan', 'Feb', 'Mar', 'Apr', "May", 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ]
+  constructor(){
+    // this.note = new Date();
+    // this.noteDetails = new NoteDetailsModule(
+    // this.note.getDay(),
+    // this.monthsArr[this.note.getMonth()],
+    // "Moro",
+    // "lorem ipsum"
+    // );
+    // console.log(this.noteDetails);
+  }
 }
