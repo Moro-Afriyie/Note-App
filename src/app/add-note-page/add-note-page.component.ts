22a86dcf96f85c5b81ec2d7577e8cdad2be999a4
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-12 10:11:46 +0000

    created a addNoteForm variable using reactive forms module

--- src/app/add-note-page/add-note-page.component.ts
+++ src/app/add-note-page/add-note-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-note-page',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-note-page.component.scss']
 })
 export class AddNotePageComponent implements OnInit {
+  addNoteForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.addNoteForm = new FormGroup({
+      title:  new FormControl('', Validators.required),
+      body: new FormControl('', Validators.required)
+    }
+     
+    );
   }
 
 }
