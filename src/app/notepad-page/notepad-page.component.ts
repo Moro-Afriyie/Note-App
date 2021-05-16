9ec1ea3e8b59bb6f53a26253ae78f17637bc8995
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-16 13:55:49 +0000

    modified the notepadList array

--- src/app/notepad-page/notepad-page.component.ts
+++ src/app/notepad-page/notepad-page.component.ts
@@ -15,9 +15,9 @@ export class NotepadPageComponent implements OnInit {
   noteDetails: NoteDetailsModule;
   // array to store a copy of  the notepadLists
   notepadListArray: NoteDetailsModule[]=[
-    new NoteDetailsModule("10", "Mar", "Title1", this.body),
-    new NoteDetailsModule("11", "Apr", "Title2", this.body),
-    new NoteDetailsModule("12", "May", "Title3", this.body),
+    // new NoteDetailsModule("10", "Mar", "Title1", this.body),
+    // new NoteDetailsModule("11", "Apr", "Title2", this.body),
+    // new NoteDetailsModule("12", "May", "Title3", this.body),
   ];
   // // array to update the new array
   // updatedArray: NoteDetailsModule[]=[
