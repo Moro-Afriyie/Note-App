9bd34afc1f9da612e8597bcdb52a7ccb9092e18e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-19 10:13:35 +0000

    created a highlight directive  to highlight text when user searches it

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -4,6 +4,10 @@ import { Directive, Input, SimpleChanges, Renderer2, ElementRef, OnChanges } fro
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  @Input() searchedWord: string; // searchText
+  @Input() content: string; // HTML content
+  @Input() classToApply: string; //class to apply for highlighting
+  @Input() setTitle = false; //sets title attribute of HTML
 
   constructor() { }
 
