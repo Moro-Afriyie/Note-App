ad5177379b4b6f64265e315d0f4350ec5a43e20e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-19 09:06:03 +0000

    implemented the search logic in the filter pipe.ts file

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,8 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  /**
+   * Transform
+   *
+   * @param {any[]} items
+   * @param {string} searchText
+   * @returns {any[]}
+   */
+   transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(it => {
+      return it.toLocaleLowerCase().includes(searchText);
+    });
   }
 
 }
