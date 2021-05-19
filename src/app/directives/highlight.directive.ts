cb3d7b68d3e1ef65cc601181b5d28b1a9e6444b6
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-19 10:21:42 +0000

    implemented the directive logic

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -28,6 +28,18 @@ export class HighlightDirective implements OnChanges{
       this.renderer.setProperty(this.el.nativeElement, 'innerHTML', this.content);
       return;
     }
+
+    this.renderer.setProperty(
+      this.el.nativeElement,
+      'innerHTML',
+      this.getFormattedText()
+    );
   }
 
-}
+  getFormattedText() {
+    const re = new RegExp(`(${this.searchedWord})`, 'gi');
+    return this.content.replace(re, `<span class="${this.classToApply}">$1</span>`);
+  }
+  }
+
+
