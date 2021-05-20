cb8d4c88fae30be35ec9353431c2079ede9bcd3f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-05-20 20:54:24 +0000

    local storage works

--- src/app/notepad-page/notepad-page.component.ts
+++ src/app/notepad-page/notepad-page.component.ts
@@ -65,9 +65,6 @@ export class NotepadPageComponent implements OnInit {
 
      // set setTimeout to allow the deletion to happen
      setTimeout(() => {
-      // this.notepadListArray.splice(index, 1); // delete the card
-      // this.notepadListArray = [...this.notepadListArray]; // update the list
-      // console.log(this.notepadListArray);
       this.notePadEditService.deleteNote(index);
     }, 3200);
 
