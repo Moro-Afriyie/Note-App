9f388a8ee90d3740372058d6989604b167ca00bf
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-06-05 10:56:43 +0000

    set the values for the various form fields

--- src/app/add-note-page/add-note-page.component.ts
+++ src/app/add-note-page/add-note-page.component.ts
@@ -32,8 +32,9 @@ export class AddNotePageComponent implements OnInit {
       console.log(res);
       // this.edit = res.edit;
       // this.index = res.index;
-      this.addNoteForm.value.title.setValue(res.note.title);
-      this.addNoteForm.value.body.setValue(res.note.body);
+      this.addNoteForm.value.setValue({title: res.note.title,
+      body: res.note.body});
+      // this.addNoteForm.value.body.setValue(res.note.body);
       console.log(this.addNoteForm.value);
       //  console.log("index: ", this.index );
       //  console.log("edit: ", this.edit);
